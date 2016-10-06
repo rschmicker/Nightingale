4ceb0b8e9da582d958159d2513fee3b9a84e1fdc
Robert Schmicker <rhschmicker@gmail.com>
2016-10-05 21:52:55 -0400

    Delted extra commented code

--- sbk/pacc.h
+++ sbk/pacc.h
@@ -1,26 +1,3 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define PACC_SIZE 64
-
-typedef struct{
-	char ascii_hex;
-	char pacc;
-}MAP;
-
-typedef struct{
-    MAP map1[PACC_SIZE];
-}PACC;
-
-void fill_ascii_to_pacc_map(PACC *p);
-
-void ascii_hex_to_pacc(PACC *p);
-
-void get_hex(int n, char* hex);
-
-int get_ascii(char c);
-*/
-
 #include <stdio.h>
 #define PACC_SIZE 64
 
