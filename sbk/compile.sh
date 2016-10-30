d2b414a27f7d612ed87fde84e10db54d9fa0f97a
Luke Lonergan <llonergan@gpuhost1.Bigdatadevelope.local>
2016-10-30 13:35:10 -0700

    Cleanup

--- sbk/compile.sh
+++
@@ -1,4 +0,0 @@
-gcc -std=c99 -I./randlib/include/ -I./randlib/extras/ -I/usr/local/openssl/include/openssl/ -c -o sbk.o sbk.c
-gcc -std=c99 -I/usr/local/openssl/include/openssl/ -c -o pacc.o pacc.c
-gcc -std=c99 -I./randlib/include/ -I./randlib/extras -c -o main.o main.c
-gcc -L./randlib/src main.o pacc.o sbk.o ./randlib/extras/entropy.o -lpcg_random -lcrypto -lssl -Wall -o main
