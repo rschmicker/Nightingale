6a39f24a591f3e82aaa6b526f91698e1bafeb5c7
Luke Lonergan <llonergan@gpuhost1.Bigdatadevelope.local>
2016-12-20 13:35:00 -0800

    Fix compiler warnings, use system std struct for timeval

--- mysecond.c
+++ mysecond.c
@@ -2,16 +2,9 @@
 
 double mysecond()
 {
-    struct timeval { long tv_sec;
-                    long tv_usec; };
-
-    struct timezone { int   tz_minuteswest;
-                    int   tz_dsttime; };     
-
     struct timeval tp;
-    struct timezone tzp;
     int i;
 
-    i = gettimeofday(&tp,&tzp);
+    i = gettimeofday(&tp, (void *)NULL);
     return ( (double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
 }
--- mysecond.h
+++ mysecond.h
@@ -5,6 +5,7 @@
    and without appended underscores, so it *should*
    automagically link with FORTRAN */
 
+#include <stdlib.h>
 #include <sys/time.h>
 
 double mysecond();
