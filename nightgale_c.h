a67b0c3f4a876a65e22bf8747471b734f2644ac3
Robert Schmicker <rhschmicker@gmail.com>
2017-03-15 11:26:17 -0700

    grab top 6 bits

--- nightgale_c.h
+++ nightgale_c.h
@@ -13,7 +13,7 @@
 #include "sub_t.h"
 
 #define WORD_SIZE 8
-#define MASK 0x3F
+#define MASK 0xfc00000000000000
 
 //-----------------------------------------------------------------------------
 // Encrypt
@@ -28,7 +28,7 @@ void decrypt_night(SUB *s, size_t len, const unsigned char *in,
 			unsigned char *out);
 
 //-----------------------------------------------------------------------------
-// Anchor rotation based off the last 6 bits of the key
+// Anchor rotation based off the first 6 bits of the key
 //-----------------------------------------------------------------------------
 inline uint64_t rotr64 (uint64_t n, unsigned int c){
     const unsigned int mask = (CHAR_BIT*sizeof(n)-1);
