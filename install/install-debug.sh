320f66d013352855789a63577b38de90214839f1
Robert Schmicker <rhschmicker@gmail.com>
2017-04-30 17:58:46 -0400

    Debug for libssl integration

---
+++ install/install-debug.sh
@@ -0,0 +1,15 @@
+PATCH=nightgale-0.9.patch
+
+wget https://github.com/openssl/openssl/archive/master.zip && \
+unzip master.zip && \
+cp $PATCH openssl-master/ && \
+cd openssl-master/ && \
+patch -p1 < $PATCH && \
+./config -d && \
+make update && \
+make -j 12 && \
+gcc -I./include -L. -Wextra -Wall -o night_evp_test night_evp_test.c -lcrypto && \
+gcc -I./include -L. -Wextra -Wall -o night_test night_test.c -lcrypto && \
+LD_LIBRARY_PATH=. ./night_test && \
+LD_LIBRARY_PATH=. ./night_evp_test && \
+LD_LIBRARY_PATH=. ./apps/openssl speed -elapsed -evp nightgale
