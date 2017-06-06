5f342b6fced223807d3a28c0bbf72e12ac1fc638
Robert Schmicker <rhschmicker@gmail.com>
2017-06-05 19:16:58 -0700

    Switched to a stable 1.1.0f release

--- install/install-ssl.sh
+++ install/install-ssl.sh
@@ -1,13 +1,20 @@
 PATCH=nightgale-ssl-0.9.patch
 
-wget https://github.com/openssl/openssl/archive/master.zip && \
-unzip master.zip && \
-cp $PATCH openssl-master/ && \
-cd openssl-master/ && \
+wget "https://www.openssl.org/source/openssl-1.1.0f.tar.gz"
+wget "https://www.openssl.org/source/openssl-1.1.0f.tar.gz.sha256"
+
+if [[sha256sum openssl-1.1.0f.tar.gz -ne $(cat openssl-1.1.0f.tar.gz.sha256)]];
+then
+    exit 1
+fi
+tar -xvf openssl-1.1.0f.tar.gz
+mv openssl-1.1.0f/ openssl/
+cp $PATCH openssl/ && \
+cd openssl/ && \
 patch -p1 < $PATCH && \
 ./config && \
 make update && \
-make -j 12 && \
+make -j $(nproc --all) && \
 gcc -I./include -L. -Wextra -Wall -o night_evp_test night_evp_test.c -lcrypto && \
 gcc -I./include -L. -Wextra -Wall -o night_test night_test.c -lcrypto && \
 LD_LIBRARY_PATH=. ./night_test && \
--- pingpong/compile.sh
+++ pingpong/compile.sh
@@ -1,7 +1,9 @@
-gcc server.c -o server -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
-gcc client.c -o client -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
-gcc server_speed_night.c -o server_speed_night -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
-gcc client_speed_night.c -o client_speed_night -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
-gcc server_speed_aes.c -o server_speed_aes -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
-gcc client_speed_aes.c -o client_speed_aes -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+CC=gcc
+
+$CC server.c -o server -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+$CC client.c -o client -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+$CC server_speed_night.c -o server_speed_night -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+$CC client_speed_night.c -o client_speed_night -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+$CC server_speed_aes.c -o server_speed_aes -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
+$CC client_speed_aes.c -o client_speed_aes -I../install/openssl/include -L../install/openssl/ -lcrypto -lssl
 
